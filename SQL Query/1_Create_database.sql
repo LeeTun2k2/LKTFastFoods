0ff9122a5ccf139135b07cb5a0f2a9f0168cbe6d
Lê Quang Tùng <lequangtung2002@gmail.com>
2022-11-27 11:56:31 +0700

    init database

---
+++ SQL Query/1_Create_database.sql
@@ -0,0 +1,121 @@
+create database LKTFastFood
+go
+use LKTFastFood
+go
+
+-- Accounts
+create table Accounts(
+	Username varchar(40) primary key,
+	Password varchar(40) not null,
+	Roles bit not null,
+	Active bit not null
+)
+go
+
+-- Clients
+create table Clients(
+	Username varchar(40) primary key references Accounts(Username),
+	Name nvarchar(40) not null,
+	Age int,
+	PhoneNumber varchar(11),
+	Email varchar(40),
+	Address nvarchar(100)
+)
+go
+
+-- Managers
+create table Managers(
+	Username varchar(40) primary key references Accounts(Username),
+	Name nvarchar(40) not null,
+	Age int,
+	PhoneNumber varchar(11),
+	Email varchar(40),
+	Address nvarchar(100)
+)
+go
+
+-- Vouchers
+create table Vouchers(
+	ID int identity(0,1) primary key,
+	Start_Date date not null,
+	End_Date date not null,
+	Discount int not null
+)
+go
+
+-- Products
+create table Products(
+	ID int identity(0,1) primary key,
+	Name nvarchar(40) not null,
+	Description nvarchar(100),
+	Amount int not null,
+	Status bit not null,
+	Import_Price int not null,
+	Export_Price int not null,
+	Voucher_ID int references Vouchers(ID)
+)
+go
+
+-- Payment_Method
+create table Payment_Methods(
+	ID int identity(0,1) primary key,
+	AccountNumber varchar(40) not null,
+	AccountName varchar(40) not null,
+	BankName varchar(40) not null
+)
+go
+
+-- Payments
+create table Payments(
+	ID int identity(0,1) primary key,
+	Method_ID int references Payment_Methods(ID),
+	DateDone date not null,
+	Amount int not null,
+	Description nvarchar(100)
+)
+go
+
+-- Orders
+create table Orders(
+	ID int identity(0,1) primary key,
+	Payment_ID int references Payments(ID),
+	Amount int not null
+)
+go
+
+-- Order_Info
+create table Order_Info (
+	ID int identity(0,1) primary key,
+	Product_ID int, 
+	Order_ID int,
+	Quantity int not null,
+	Amount int not null,
+	foreign key(Product_ID) references Products(ID),
+	foreign key(Order_ID) references Orders(ID)
+)
+go
+
+-- Assessments
+create table Assessments (
+	ID int identity(0,1) primary key,
+	Order_ID int references Orders(ID),
+	Stars float not null
+)
+go 
+
+-- Menu
+create table Menu (
+	ID int identity(0,1) primary key,
+	Product_ID int references Products(ID)
+)
+go 
+
+-- Order_History
+create table Order_History (
+	ID int identity(0,1) primary key,
+	Client_ID varchar(40),
+	Order_ID int,
+	foreign key(Client_ID) references Clients(Username),
+	foreign key(Order_ID) references Orders(ID)
+)
+go
---
+++ SQL Query/4_Procedure.sql
@@ -0,0 +1,514 @@
+use LKTFastFood
+go
+
+-- Accounts
+create procedure AddAccount @username varchar(40), @password varchar(40), @role bit, @active bit
+as
+	insert into Accounts values(@username, @password, @role, @active)
+go
+
+create procedure UpdateAccount @username varchar(40), @password varchar(40), @role bit, @active bit
+as
+	update Accounts 
+	set Password = @password,
+		Roles = @role,
+		Active = @active
+	where Username = @username
+go
+
+create procedure GetAllAccounts 
+as 
+	select * from Accounts
+go
+
+create procedure GetOneAccount @username varchar(40)
+as 
+	select * from Accounts
+	where Username = @username
+go
+
+
+
+-- Clients
+create procedure AddClient 
+	@username varchar(40), 
+	@name nvarchar(40), 
+	@age int, 
+	@phoneNumber varchar(11), 
+	@email varchar(40), 
+	@address nvarchar(100)
+as
+	insert into Clients values(@username, @name, @age, @phoneNumber, @email, @address)
+go
+
+create procedure UpdateClient 
+	@username varchar(40), 
+	@name nvarchar(40), 
+	@age int, 
+	@phoneNumber varchar(11), 
+	@email varchar(40), 
+	@address nvarchar(100)
+as
+	update Clients 
+	set Name = @name,
+		Age = @age,
+		PhoneNumber = @phoneNumber,
+		Email = @email,
+		Address = @address
+	where Username = @username
+go
+
+create procedure DeleteClient @username varchar(40)
+as
+begin transaction
+	update Order_History
+	set Client_ID = 'null'
+	where Order_History.Client_ID = @username
+
+	delete from Clients
+	where Username = @username
+commit
+go
+
+create procedure GetAllClients 
+as 
+	select * from Clients
+go
+
+create procedure GetOneClient @username varchar(40)
+as 
+	select * from Clients
+	where Username = @username
+go
+
+
+
+-- Managers
+create procedure AddManager
+	@username varchar(40), 
+	@name nvarchar(40), 
+	@age int, 
+	@phoneNumber varchar(11), 
+	@email varchar(40), 
+	@address nvarchar(100)
+as
+	insert into Managers values(@username, @name, @age, @phoneNumber, @email, @address)
+go
+
+create procedure UpdateManager
+	@username varchar(40), 
+	@name nvarchar(40), 
+	@age int, 
+	@phoneNumber varchar(11), 
+	@email varchar(40), 
+	@address nvarchar(100)
+as
+	update Managers 
+	set Name = @name,
+		Age = @age,
+		PhoneNumber = @phoneNumber,
+		Email = @email,
+		Address = @address
+	where Username = @username
+go
+
+create procedure DeleteManager @username varchar(40)
+as
+	delete from Managers
+	where Username = @username
+go
+
+create procedure GetAllManagers 
+as 
+	select * from Managers
+go
+
+create procedure GetOneManager @username varchar(40)
+as 
+	select * from Managers
+	where Username = @username
+go
+
+
+
+-- Vouchers
+create procedure AddVoucher @startDate date, @endDate date, @discount int
+as
+	insert into Vouchers values(@startDate, @endDate, @discount)
+go
+
+create procedure UpdateVoucher @id int, @startDate date, @endDate date, @discount int
+as
+	update Vouchers 
+	set Start_Date = @startDate,
+		End_Date = @endDate,
+		Discount = @discount
+	where ID = @id
+go
+
+create procedure DeleteVoucher @id int
+as
+begin transaction
+	update Products
+	set Voucher_ID = 0
+	where Products.Voucher_ID = @id
+
+	delete from Vouchers
+	where ID = @id
+commit
+go
+
+create procedure GetAllVouchers 
+as 
+	select * from Vouchers
+go
+
+create procedure GetOneVoucher @id int
+as 
+	select * from Vouchers
+	where ID = @id
+go
+
+
+
+-- Products
+create procedure AddProduct 
+	@name nvarchar(40),
+	@description nvarchar(40),
+	@status bit,
+	@importPrice int,
+	@exportPrice int,
+	@voucherID int
+as
+	insert into Products values(@name, @description, 0, @status, @importPrice, @exportPrice, @voucherID)
+go
+
+create procedure UpdateProduct 
+	@ID int,
+	@name nvarchar(40),
+	@description nvarchar(40),
+	@status bit,
+	@importPrice int,
+	@exportPrice int,
+	@voucherID int
+as
+	update Products 
+	set Name = @name,
+		Description = @description,
+		Status = @status,
+		Import_Price = @importPrice,
+		Export_Price = @exportPrice,
+		Voucher_ID = @voucherID
+	where ID = @id
+go
+
+create procedure DeleteProduct @id int
+as
+begin transaction
+	update Menu
+	set Product_ID = 0
+	where Menu.Product_ID = @id
+
+	update Order_Info
+	set Product_ID = 0
+	where Order_Info.Product_ID = @id
+
+	delete from Products
+	where ID = @id
+commit
+go
+
+create procedure GetAllProducts 
+as 
+	select * from Products
+go
+
+create procedure GetOneProduct @id int
+as 
+	select * from Products
+	where ID = @id
+go
+
+
+
+-- Order_Info
+create procedure AddOrder_Info 
+	@productID int, 
+	@orderID int, 
+	@quantity int
+as
+	insert into Order_Info values(@productID, @orderID, @quantity, 0)
+go
+
+create procedure UpdateOrder_Info 
+	@id int, 
+	@productID int, 
+	@orderID int, 
+	@quantity int
+as
+	update Order_Info 
+	set Product_ID = @productID,
+		Order_ID = @orderID,
+		Quantity = @quantity
+	where ID = @id
+go
+
+create procedure DeleteOrder_Info @id int
+as
+	delete from Order_Info
+	where ID = @id
+go
+
+create procedure GetAllOrder_Infos 
+as 
+	select * from Order_Info
+go
+
+create procedure GetOneOrder_Info @id int
+as 
+	select * from Order_Info
+	where ID = @id
+go
+
+
+
+-- Payment_Method
+create procedure AddPayment_Method @accountNumber varchar(40), @accountName varchar(40), @bankName varchar(40)
+as
+	insert into Payment_Methods values(@accountNumber, @accountName, @bankName)
+go
+
+create procedure UpdatePayment_Method @id int, @accountNumber varchar(40), @accountName varchar(40), @bankName varchar(40)
+as
+	update Payment_Methods 
+	set AccountNumber = @accountNumber,
+		AccountName = @accountName,
+		BankName = @bankName
+	where ID = @id
+go
+
+create procedure DeletePayment_Method @id int
+as
+begin transaction
+	update Payments
+	set Method_ID = 0
+	where Payments.Method_ID = @id
+
+	delete from Payment_Methods
+	where ID = @id
+commit
+go
+
+create procedure GetAllPayment_Methods
+as 
+	select * from Payment_Methods
+go
+
+create procedure GetOnePayment_Method @id int
+as 
+	select * from Payment_Methods
+	where ID = @id
+go
+
+
+
+-- Payments
+create procedure AddPayment @methodID int, @dateDone date, @amount int, @description nvarchar(100)
+as
+	insert into Payments values(@methodID, @dateDone, @amount, @description)
+go
+
+create procedure UpdatePayment @id int, @methodID int, @dateDone date, @amount int, @description nvarchar(100)
+as
+	update Payments 
+	set Method_ID = @methodID,
+		DateDone = @dateDone,
+		Amount = @amount,
+		Description = @description
+	where ID = @id
+go
+
+create procedure DeletePayment @id int
+as 
+begin transaction
+	update Orders
+	set Payment_ID = 0
+	where Orders.Payment_ID =@id
+
+	delete from Payments
+	where ID = @id
+commit
+go
+
+create procedure GetAllPayments
+as 
+	select * from Payments
+go
+
+create procedure GetOnePayment @id int
+as 
+	select * from Payments
+	where ID = @id
+go
+
+
+-- Orders
+create procedure AddOrder @paymentID int, @amount int
+as
+	insert into Orders values(@paymentID, @amount)
+go
+
+create procedure UpdateOrder @id int, @paymentID int, @amount int
+as
+	update Orders 
+	set Payment_ID = @paymentID,
+		Amount = @amount
+	where ID = @id
+go
+
+create procedure DeleteOrder @id int
+as
+begin transaction
+	update Order_Info
+	set Order_ID = 0
+	where Order_ID = @id
+
+	update Order_History
+	set Order_ID = 0
+	where Order_ID = @id
+
+	update Assessments
+	set Order_ID = 0
+	where Order_ID = @id
+
+	delete from Orders
+	where ID = @id
+commit
+go
+
+create procedure GetAllOrders
+as 
+	select * from Orders
+go
+
+create procedure GetOneOrder @id int
+as 
+	select * from Orders
+	where ID = @id
+go
+
+
+
+-- Assessments
+create procedure AddAssessment @orderID int, @stars float
+as
+	insert into Assessments values(@orderID, @stars)
+go
+
+create procedure UpdateAssessment @id int, @orderID int, @stars float
+as
+	update Assessments 
+	set Order_ID = @orderID,
+		Stars = @stars
+	where ID = @id
+go
+
+create procedure DeleteAssessment @id int
+as
+	delete from Assessments
+	where ID = @id
+go
+
+create procedure GetAllAssessments
+as 
+	select * from Assessments
+go
+
+create procedure GetOneAssessment @id int
+as 
+	select * from Assessments
+	where ID = @id
+go
+
+
+
+-- Menu
+create procedure AddMenu @productID int
+as
+	insert into Menu values(@productID)
+go
+
+create procedure UpdateMenu @id int, @productID int
+as
+	update Menu 
+	set Product_ID = @productID
+	where ID = @id
+go
+
+create procedure DeleteMenu @id int
+as
+	delete from Menu
+	where ID = @id
+go
+
+create procedure GetAllMenu
+as 
+	select * from Menu
+go
+
+create procedure GetOneMenu @id int
+as 
+	select * from Menu
+	where ID = @id
+go
+
+
+
+-- Order_History
+create procedure AddOrder_History @clientID varchar(40), @orderID int
+as
+	insert into Order_History values(@clientID, @orderID)
+go
+
+create procedure UpdateOrder_History @id int, @clientID varchar(40), @orderID int
+as
+	update Order_History 
+	set Client_ID = @clientID,
+		Order_ID = @orderID
+	where ID = @id
+go
+
+create procedure DeleteOrder_History @id int
+as
+	delete from Order_History
+	where ID = @id
+go
+
+create procedure GetAllOrder_History
+as 
+	select * from Order_History
+go
+
+create procedure GetOneOrder_History @id int
+as 
+	select * from Order_History
+	where ID = @id
+go
+
+
+
+-- Delete Account
+create procedure DeleteAccount @username varchar(40)
+as
+begin transaction
+	declare @roles bit
+	set @roles = (select roles from Accounts where Username = @username)
+	if @roles = 1
+		exec DeleteManager @username
+	else
+		exec DeleteClient @username
+
+	delete from Accounts
+	where Username = @username
+commit
+go
